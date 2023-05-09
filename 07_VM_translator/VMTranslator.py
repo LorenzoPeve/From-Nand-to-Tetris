773007122b1590fea93b992743e77a233e183dec
Lorenzo Peve <lpeve01@gmail.com>
2023-05-09 12:29:08 -0500

    Update VMTranslator.py

--- 07_VM_translator/VMTranslator.py
+++ 07_VM_translator/VMTranslator.py
@@ -4,33 +4,47 @@ from reader import Reader
 from operations import Operation
 from memory import  MemorySegment
 
-def write(fname, data: list[str]):
-    with open(fname, 'w') as file:
-        for string in data:
-            file.write(string + '\n')
+
+
 
 r = Reader(sys.argv[1])
+
+# import os
+# print(os.listdir())
+# r = Reader(r'.\07_VM_translator\input\lp_test.vm')
+# fname=sys.argv[1].split('\\')[1][:-3]
+
+
+print(f'Input File {sys.argv[1]}')
+fname = sys.argv[1].split('.')[0]
+print(f'Name {fname}')
 data = r.read()
 
 translated = []
 for line in data:
-
+    translated.append(f'// {line}')
     try:
         op = Operation(line)
         t = op.translate_operations()
 
     except ValueError as e:
         try:
-            m = MemorySegment(line)
+            m = MemorySegment(line, fname)
             t = m.translate_memory()
         except Exception as e:
-            print(f'Line: {line}')
-            print(e)
+            raise Exception
     finally:
         translated.extend(t)
 
 translated = [t for t in translated if t != '']
 translated.extend(['(END)', '@END', '0;JMP']) # Infinite loop
 
-write(r'output\\' + sys.argv[1].split('\\')[1][:-3] + '.asm', translated)
+with open(fname + '.asm', 'w') as file:
+    for i, string in  enumerate(translated):
+        if i == len(translated)-1:
+            file.write(string)
+            continue
+        file.write(string + '\n')
 
+with open(fname + '.asm', 'r') as file:
+    print(file.readlines())
