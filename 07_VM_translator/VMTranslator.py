be6dce804408de03c55f9949f5d3f098d3b4e5df
Lorenzo Peve <lpeve01@gmail.com>
2023-11-07 01:10:52 -0600

    100/100

--- 07_VM_translator/VMTranslator.py
+++ 07_VM_translator/VMTranslator.py
@@ -15,14 +15,18 @@ if path_obj.is_dir():
         asm_code += t.translate()
 
     asm_code = asm_code + '(END)\n@END\n0;JMP\n'
-    with open(f'{path_obj.name}.asm', "w") as file:
+
+    # Output to same directory
+    out_path = os.path.join(path_obj, f'{path_obj.name}.asm')
+    with open(out_path, "w") as file:
         file.write(asm_code)
 
 elif path_obj.is_file():
 
-    t = Translator(path_obj)
+    t = Translator(path_obj)    
     asm_code = t.translate()
     asm_code = asm_code + '(END)\n@END\n0;JMP\n'
     
-    with open(f'{path_obj.stem}.asm', "w") as file:
+    out_path = os.path.join(os.path.dirname(path_obj), f'{path_obj.stem}.asm')
+    with open(out_path, "w") as file:
         file.write(asm_code)
