28f19ba92cff8e545cd93c66c0e32507adbcb7f8
Lorenzo Peve <lpeve01@gmail.com>
2023-11-11 12:07:17 -0600

    passing_all_tests

--- 08_VM_Translator_II/VMTranslator.py
+++ 08_VM_Translator_II/VMTranslator.py
@@ -1,128 +1,38 @@
-import sys
 import os
+from pathlib import Path
+import sys
 
-from reader import Reader
-from operations import Operation
-from memory import MemorySegment
-from branching import  Branch
-from functions import Function
+from translator import Translator
+from mapper import routine_call_f
 
+path_obj = Path(sys.argv[1])
 
-def _get_filename(filepath) -> str:
-    return os.path.basename(filepath)
-
-
-def _write_new_file_with_extension(filepath, data):
-    """
-    Writes a new file with the same name as the given file and
-    ".asm" extension.
-    """
-    filename, _ = os.path.splitext(filepath)
-    new_path = filename + '.asm'
-
-    with open(new_path, "w") as file:
-        for i, string in enumerate(data):
-            if i == len(data)-1:
-                file.write(string)
-                continue
-            file.write(string + '\n')
+if path_obj.is_dir():
+    files = [f for f in os.listdir(path_obj) if f[-3:] == '.vm']
 
 
-def _is_arith_operation(s: str) -> bool:
-    """Returns True if line is an arithmetic operation."""
-    if s in Operation.allowed:
-        return True
-    return False
+    # Booting: SP = 256
+    asm_code = '@256\nD=A\n@0\nM=D\n'
+    # Calling Sys.init
+    asm_code += routine_call_f('Sys.init', 0, 0)
 
+    for file in files:
+        t = Translator(os.path.join(path_obj, file))
+        asm_code += t.translate()
 
-def _is_memory_alloc(s: str) -> bool:
-    """Returns True if line is a memory operation."""
-    if s.startswith('push') or s.startswith('pop'):
-        return True
-    return False
+    asm_code = asm_code + '(END)\n@END\n0;JMP\n'
 
+    # Output to same directory
+    out_path = os.path.join(path_obj, f'{path_obj.name}.asm')
+    with open(out_path, "w") as file:
+        file.write(asm_code)
 
-def _is_function(s: str) -> bool:
-    """Returns True if line is a function definition or function call."""
-    if (s.startswith('call') or
-        s.startswith('function') or
-        s.startswith('return')):
+elif path_obj.is_file():
 
-        return True
-    return False
-
-
-def _is_branching_step(s: str) -> bool:
-    """Returns True if line is a branching command."""
-    if (s.startswith('label') or
-        s.startswith('if-goto') or
-            s.startswith('goto')):
-
-        return True
-    return False
-
-
-def translate_file(filepath: str):  # -> list[str]
-    """Initializes reader and translates a .vm file into a list[str]"""
-    r = Reader(filepath)
-    fname = _get_filename(filepath)
-    data = r.read()
-
-    translated = []
-    for line in data:
-        translated.append(f'// {line}')
-
-        if _is_arith_operation(line):
-            op = Operation(line)
-            t = op.translate_operations()
-
-        elif _is_memory_alloc(line):
-            m = MemorySegment(line, fname)
-            t = m.translate_memory()
-
-        elif _is_branching_step(line):
-            b = Branch(line)
-            t = b.translate_branch()
-
-        elif _is_function(line):
-            f = Function(line)
-            t = f.translate()
-
-        else:
-            raise Exception(f'Line couldnt be translated: {line}')
-
-        translated.extend(t)
-
-    translated = [t for t in translated if t != '']
-    return translated
-
-
-is_dir = os.path.isdir(sys.argv[1])
-
-if not is_dir:    
-    translated = translate_file(sys.argv[1])
-    translated.extend(['(END)', '@END', '0;JMP'])  # Infinite loop
-    print(translated)
-    _write_new_file_with_extension(sys.argv[1], translated)
-
-else:
-    vm_files = [f for f in os.listdir(sys.argv[1]) if f.endswith('.vm')]
+    t = Translator(path_obj)    
+    asm_code = t.translate()
+    asm_code = asm_code + '(END)\n@END\n0;JMP\n'
     
-    # Booting Steps
-    translated = ['// Booting: SP = 256']    
-    translated.extend(['@256', 'D=A', '@0', 'M=D'])
-        
-    # Calling Sys.init Steps
-    translated.append('// Call Sys.init')
-    f = Function('call Sys.init 0')
-    translated.extend(f.translate())
-
-    translated.append('// Finished Booting')
-    for file in vm_files:
-        filepath = os.path.join(sys.argv[1], file)
-        t = translate_file(filepath)
-        translated.extend(t)
-
-    translated.extend(['(END)', '@END', '0;JMP'])
-    path = os.path.join(sys.argv[1], sys.argv[1] + '.vm')
-    _write_new_file_with_extension(path, translated)
+    out_path = os.path.join(os.path.dirname(path_obj), f'{path_obj.stem}.asm')
+    with open(out_path, "w") as file:
+        file.write(asm_code)
---
+++ 08_VM_Translator_II/translator.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+
+import mapper
+
+class Reader():
+
+    def __init__(self, data):
+        self.data = data
+
+    def _remove_whitespace(self):
+        return [line.strip() for line in self.data]
+
+    def _remove_empty_lines(self):
+        return [line for line in self.data if len(line) > 1]
+
+    def _remove_comment_lines(self):
+        return [line for line in self.data if not line.startswith('//')]
+
+    def _remove_inline_comments(self):
+        for i, line in enumerate(self.data):
+            if '//' in line:
+                idx = line.find('//')
+                line = line[:idx]
+                self.data[i] = line.strip()
+
+    def read(self):
+        self.data = self._remove_whitespace()
+        self.data = self._remove_empty_lines()
+        self.data = self._remove_comment_lines()
+        self._remove_inline_comments()
+        return self.data
+
+class Translator():
+
+    def __init__(self, fpath):
+        
+        self.fpath = fpath
+        path = Path(fpath)
+        self.filename = path.stem
+
+        with open(self.fpath) as f:
+            data = f.readlines()
+            self.instructions = Reader(data).read()
+ 
+    def translate(self):
+        """Translates the instruction in a single file into `.asm` commands."""
+        asm_code = ""
+        for idx, line in enumerate(self.instructions):
+            asm_code += mapper.translate_line(line, idx)
+        
+        asm_code = asm_code.replace('@FILENAME.', f'@{self.filename}.')
+        asm_code = asm_code.replace('(FILENAME.', f'({self.filename}.')
+        return asm_code
